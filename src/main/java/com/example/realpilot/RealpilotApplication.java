b430c817380838914c3c88daddb963cba69fe930
jiyoung917 <jiyoung917@zuminternet.com>
2019-05-20 14:12:49 +0900

    first commit

---
+++ src/main/java/com/example/realpilot/RealpilotApplication.java
@@ -0,0 +1,13 @@
+package com.example.realpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealpilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealpilotApplication.class, args);
+    }
+
+}
