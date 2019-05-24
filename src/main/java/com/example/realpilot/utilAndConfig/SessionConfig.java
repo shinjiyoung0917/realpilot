85fdaa13e0081179f60e80e66084b3ae33f6e683
jiyoung917 <jiyoung917@zuminternet.com>
2019-05-24 18:07:49 +0900

    @PostConstruct로 샘플 지역 데이터 DB에 로드 완료

--- src/main/java/com/example/realpilot/utilAndConfig/SessionConfig.java
+++ src/main/java/com/example/realpilot/utilAndConfig/SessionConfig.java
@@ -41,5 +41,4 @@ public class SessionConfig {
         if (NEO4J_URL==null) NEO4J_URL=System.getProperty("NEO4J_URL","jdbc:neo4j:http://localhost:11002");
         return new DriverManagerDataSource(NEO4J_URL);
     }
-
 }
